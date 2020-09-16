4b3306c0a8bf5926b4d2037d2a7da64104951895
opilane <opilane@HP-CZC6297B8Q>
2020-09-16 09:37:08 +0300

    Scaffolded

--- Mootorratas/Program.cs
+++ Mootorratas/Program.cs
@@ -10,8 +10,25 @@ namespace Mootorrattas
     {
         public int Id;
         protected string _Mark;
+        protected string _Model;
+        private int _TopSpeed;
+        private decimal _FuelType;
+        private int _Mileage;
+        protected string _Color;
 
+        private static decimal _FuelType = 98;
+        private static int _Mileage = 0;
+
+        public Mootorrattas(int TopSpeed, decimal FuelType, int Mileage)
+        {
+            _TopSpeed = TopSpeed;
+            _FuelType = FuelType;
+            _Mileage = Mileage; 
+        }
     }
+
+
+
     class Program
     {
         static void Main(string[] args)
