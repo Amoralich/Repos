dcb95eb3a0bea854ed9915c8f932343f0a3573cf
opilane <opilane@HP-CZC6297B8Q>
2020-09-16 09:53:57 +0300

    Create the Model

---
+++ Mootorrattas/Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mootorrattas
+{
+    public class Model
+    {
+        public int ID { get; set; }
+        protected string _Mark { get; set; }
+        protected string _Model { get; set; }
+        private int _Topspeed { get; set; }
+        private decimal _FuelType { get; set; }
+
+
+    }
+}
